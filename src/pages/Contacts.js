4b4d7daa7a5659554721243a0c02986b4519929a
FOMazart <apakistanov071@gmail.com>
2025-04-26 17:39:14 +0500

    Первый коммит

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Almaty, Kazakhstan</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram / WhatsApp</h2>
+						<p>
+							<a href="tel:+79051234567">+7 (747) 570 70 04</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:webdev@protonmail.com">
+								pakistanovazamat2@gmail.com
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
